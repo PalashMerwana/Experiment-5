06c79a575ca691129aa71c9bf5b01c5f30468ddc
PalashMerwana <palash.merwana.btech2023@sitpune.edu.in>
2024-08-31 11:27:00 +0530

    Add files via upload

---
+++ calculator_switch.cpp
@@ -0,0 +1,39 @@
+//Palash Merwana 
+//Prn: 23070123095
+//experiment 5
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char oper;
+    cout<<"enter an operator(+,-,*,/): \n";
+    cin >> oper; 
+
+    float a,b;
+    cout<<"enter two numbers: ";
+    cin>>a>>b;
+
+    switch(oper)
+    {
+        case '+':
+        cout<<a<<"+"<<b<<"="<<a+b<<endl;
+        break;
+
+        case '-':
+        cout<<a<<"-"<<b<<"="<<a-b<<endl;
+        break;
+
+        case '*':
+        cout<<a<<"*"<<b<<"="<<a*b<<endl;
+        break;
+
+        case '/':
+        cout<<a<<"/"<<b<<"="<<a/b<<endl;
+        break;
+    }
+
+
+    return 0;
+}
---
+++ greatest_number_nested.cpp
@@ -0,0 +1,42 @@
+//Palash Merwana 
+//Prn: 23070123095
+/*experiment 5*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter a: ";
+    cin>>a;
+    cout<<"enter b: ";
+    cin>>b;
+    cout<<"enter c: ";
+    cin>>c;
+
+    if(a>b)
+    {
+        if (a>c)
+        {
+            cout<<"a is the greatest";
+        }
+        else
+        {
+            cout<<"c is the greatest";
+        }
+    }
+    else
+    {
+        if (b>a && b>c)
+        {
+            cout<<"b is the greatest";
+        }
+        
+        else
+        {
+            cout<<"c is the greatest";
+        }
+    }
+
+    return 0;
+}
